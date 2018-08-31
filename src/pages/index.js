baf415bc98110e55c385c3b2b3096d0e923d8725
Juan Garcia <juan_garcia_1995@hotmail.com>
2018-08-31 15:05:53 -0500

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hello fellow coders</h1>
+    <p>My name is Juan and I'm a Front end developer.</p>
+    <p>This is my first hackathon and very excited to participate!</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
