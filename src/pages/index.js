ba5c7125f789b5fe99e549f37843c03751efd548
Juan Garcia <juan_garcia_1995@hotmail.com>
2018-09-01 14:30:31 -0500

    add link to codepen profile

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ const IndexPage = () => (
   <div>
     <h1>Hello fellow coders</h1>
     <p>My name is Juan and I'm a Front end developer.</p>
+    <p>I like to stay active on <a href="https://codepen.io/-J0hn-/">Codepen</a> by building pens with what I learned</p>
     <p>This is my first hackathon and very excited to participate!</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
