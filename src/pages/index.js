073631fb6e52f61b1df8ae2fc3192f02b1b2169f
Juan Garcia <juan_garcia_1995@hotmail.com>
2018-09-01 14:33:19 -0500

    make codepen link open in a new tab

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,13 @@ const IndexPage = () => (
   <div>
     <h1>Hello fellow coders</h1>
     <p>My name is Juan and I'm a Front end developer.</p>
-    <p>I like to stay active on <a href="https://codepen.io/-J0hn-/">Codepen</a> by building pens with what I learned</p>
+    <p>
+      I like to stay active on{' '}
+      <a href="https://codepen.io/-J0hn-/" target="_blank">
+        Codepen
+      </a>{' '}
+      by building pens with what I learned
+    </p>
     <p>This is my first hackathon and very excited to participate!</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
